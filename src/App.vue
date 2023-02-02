bd1334bdd8bb2ac7d558a57acdfcb38c0fa551b9
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-02-02 15:43:23 +0300

    Complete front end using Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <nav>-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </nav>-->
+    <NavBar/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  font-family: Arial;
+}
+
+#page-wrap {
+  margin: auto;
+  max-width: 800px;
+  min-height: 100vh;
+}
+
+button {
+  background-color: black;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  outline: 0;
+  padding: 16px;
 }
 </style>
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+
+</script>
---
+++ src/components/ProductsGrid.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="grid-wrap">
+    <ProductsGridItem
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script>
+import ProductsGridItem from "@/components/ProductGridItem.vue";
+
+export default {
+  name: "ProductsGrid",
+  props: ['products'],
+  components: {ProductsGridItem}
+}
+</script>
+
+<style scoped>
+.grid-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
+
+.product-item {
+  align-items: center;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px #888;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2%;
+  padding: 20px;
+  position: relative;
+  width: 32%;
+}
+
+</style>
